808c686cd04f139632de3cd469e8e6d9018b2fa9
ifritJP <15117169+ifritJP@users.noreply.github.com>
2020-06-11 08:13:35 +0900

    add console.

---
+++ console.go
@@ -0,0 +1,41 @@
+// Package helloworld provides a set of Cloud Functions samples.
+package main
+
+import (
+    //"encoding/json"
+    //"fmt"
+    "net"
+    "io"
+    //"strings"
+    //"bytes"
+    //"net/http"
+    //"strconv"
+    //	"context"
+	"log"
+)
+
+
+func StartConsole( hostInfo HostInfo ) {
+    server := hostInfo.toStr()
+    log.Print( "start console --- ", server )
+	local, err := net.Listen("tcp", server )
+	if err != nil {
+		log.Fatal( err )
+	}
+	defer local.Close()
+	for {
+		conn, err := local.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+        log.Print("connected")
+        go func(stream net.Conn) {
+            defer stream.Close()
+            ConsoleService( stream )
+        }(conn)
+	}
+}
+
+func ConsoleService( stream io.ReadWriteCloser ) {
+    DumpSession( stream )
+}
